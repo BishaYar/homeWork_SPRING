172e661728b20b468ee86a3a04a320bd0ae44905
Bisha <ogonia07@mail.ru>
2025-11-29 20:40:40 +0300

    проверьте ДЗ по коллекции List

--- mod2_HW2_incup/src/org/skypro/skyshop/App.java
+++ mod2_HW2_incup/src/org/skypro/skyshop/App.java
@@ -1,77 +1,113 @@
 package org.skypro.skyshop;
 
 import org.skypro.skyshop.article.Article;
+import org.skypro.skyshop.basket.ProductBasket;
 import org.skypro.skyshop.product.DiscountedProduct;
+import org.skypro.skyshop.product.Product;
 import org.skypro.skyshop.product.SimpleProduct;
-import org.skypro.skyshop.search.BestResultNotFoundException;
 import org.skypro.skyshop.search.SearchEngine;
+
+import java.util.LinkedList;
 
 public class App {
     public static void main(String[] args) {
 
-        SearchEngine searchEngine = new SearchEngine(10);
+        SearchEngine searchEngine = new SearchEngine();
 
-        System.err.println("==== Демонстрация проверки данных ПУСТОЙ СТРОКИ class SimpleProduct ====");
+        SimpleProduct prod1 = null;
         try {
-            SimpleProduct prod1 = new SimpleProduct("", 70);
+            prod1 = new SimpleProduct("печенье", 70);
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
         }
 
-        System.err.println("==== Демонстрация проверки данных NULL class SimpleProduct ====");
+        SimpleProduct prod2 = null;
         try {
-            SimpleProduct prod2 = new SimpleProduct(null, 1);
+            prod2 = new SimpleProduct("молоко", 50);
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
         }
 
-        System.err.println("==== Демонстрация проверки цены class SimpleProduct ====");
+        SimpleProduct prod3 = null;
         try {
-            SimpleProduct prod2 = new SimpleProduct("молоко", 0);
+            prod3 = new SimpleProduct("мед", 150);
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
         }
 
-        System.err.println("==== Демонстрация проверки данных завешенного % скидки class DiscountedProduct ====");
+        SimpleProduct prod6 = new SimpleProduct("кофе растворимый", 100);
+        SimpleProduct prod7 = new SimpleProduct("кофе зерновой", 150);
+
+        DiscountedProduct prod4 = null;
         try {
-            DiscountedProduct prod4 = new DiscountedProduct("печенье", 50,120);
+            prod4 = new DiscountedProduct("колбаса", 120, 10);
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
         }
 
-        System.err.println("==== Демонстрация проверки цены class DiscountedProduct ====");
+        DiscountedProduct prod5 = null;
         try {
-            DiscountedProduct prod5 = new DiscountedProduct("рыба", 0, 70);
+            prod5 = new DiscountedProduct("кофе", 100, 20);
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
         }
+
+        ProductBasket basket1 = new ProductBasket();
+        //1 Добавление продукта в корзину.
+        basket1.addProductInBasket(prod1);
+        basket1.addProductInBasket(prod2);
+        basket1.addProductInBasket(prod3);
+        basket1.addProductInBasket(prod4);
+        basket1.addProductInBasket(prod5);
+        basket1.addProductInBasket(prod6);
+        basket1.addProductInBasket(prod7);
+
+        System.out.println("=== Печать содержимого корзины с товарами ===");
+        basket1.printBasket();
+
+        System.out.println("=== Печать удаленных товаров, товар есть в корзине ===");
+        printEmptyDelList(basket1.deleteProductInBasket("мед"));
+
+        basket1.listDelProduct.clear();
+
+        System.out.println("=== Печать удаленных товаров, товара нет в корзине ===");
+        printEmptyDelList(basket1.deleteProductInBasket("бур"));
+
+        System.out.println("=== Печать содержимого корзины ===");
+        basket1.printBasket();
 
         Article article1 = new Article("кофе", "кофе темной обжарки, с кислинкой");
         Article article2 = new Article("кофе", "кофе растворимый. кофе ароматный.");
         Article article3 = new Article("кофе", "кофе в капсулах, ароматный, вкусный");
         Article article4 = new Article("кофе", "кофе растворимый, кофе гранулированный, 180гр");
+        Article article5 = new Article("кофе", "крем-кофе, кофе 100гр");
+        Article article6 = new Article("кофе", "кофе зерновой");
+        Article article7 = new Article("чай", "чай черный");
 
         searchEngine.addSearchable(article1);
         searchEngine.addSearchable(article2);
         searchEngine.addSearchable(article3);
         searchEngine.addSearchable(article4);
+        searchEngine.addSearchable(article5);
+        searchEngine.addSearchable(article6);
+        searchEngine.addSearchable(article7);
 
         //результат поиска
         System.out.println("==================== ПОЛОЖИТЕЛЬНЫЙ результат поиска =====================");
         String searchStr = "кофе";
-        try {
-            System.out.println(searchEngine.findSearchableObj(searchStr).getText());
-        } catch (BestResultNotFoundException e) {
-            e.printStackTrace();
-        }
+        System.out.println(searchEngine.findSearchableObj(searchStr));
 
-        System.err.println("==================== ОТРИЦАТЕЛЬНЫЙ результат поиска =====================");
+        System.out.println("==================== ОТРИЦАТЕЛЬНЫЙ результат поиска =====================");
         searchStr = "бур";
-        try {
-            searchEngine.findSearchableObj(searchStr);
-        } catch (BestResultNotFoundException e) {
-            e.printStackTrace();
-        }
+        System.out.println(searchEngine.findSearchableObj(searchStr));
+
+    }
+
+    public static void printEmptyDelList(LinkedList<Product> list){
+        if (list.isEmpty()) {
+            System.out.println("Список пуст");
+        } else
+            System.out.println(list);
     }
 }
 
--- mod2_HW2_incup/src/org/skypro/skyshop/search/SearchEngine.java
+++ mod2_HW2_incup/src/org/skypro/skyshop/search/SearchEngine.java
@@ -1,61 +1,21 @@
 package org.skypro.skyshop.search;
 
-public class SearchEngine {
-    public Searchable[] searchables;
+import java.util.ArrayList;
 
-    public SearchEngine(int size) {
-        searchables = new Searchable[size];
-    }
+public class SearchEngine {
+    ArrayList<Searchable> listSearchables = new ArrayList<>();
 
     public void addSearchable(Searchable search){
-        for (int i = 0; i < searchables.length; i++) {
-            if (searchables[i] == null) {
-                searchables[i] = search;
-                break;
-            } else {
-                if (searchables[i].getText().equals(search.getText())) {
-                    searchables[i] = search;
-                    break;
-                }
-            }
-        }
+        listSearchables.add(search);
     }
 
-    public Searchable findSearchableObj(String search) {
-        int [] maxRepeat = new int[searchables.length];
-        int max = 0;
-        Searchable searchFind;
-
-        for (int i = 0; i < searchables.length; i++) {
-            if (searchables[i] != null) {
-                maxRepeat[i] = getSearchTerm(searchables[i].toString(), search);
-            } else maxRepeat[i] = 0;
-        }
-
-        int index = -1;
-        for (int i = 0; i < searchables.length; i++){
-            if (max < maxRepeat[i]) {
-                max = maxRepeat[i];
-                index = i;
+    public ArrayList<Searchable> findSearchableObj(String search) {
+        ArrayList<Searchable> findListSearch = new ArrayList<>();
+        for (Searchable el : listSearchables){
+            if (el != null && el.getText().contains(search)) {
+                findListSearch.add(el);
             }
         }
-
-        if (index == -1) {
-            throw new BestResultNotFoundException("Совпадений не найдено для '" + search + "'");
-        } else searchFind = searchables[index];
-
-        return searchFind;
-    }
-
-    public int getSearchTerm(String strObj, String strFind) {
-        int index = 0;
-        int count = 0;
-        int indexStr = strObj.indexOf(strFind, index);
-        while (indexStr != -1) {
-            count++;
-            index = indexStr + strFind.length();
-            indexStr = strObj.indexOf(strFind, index);
-        }
-        return count;
+        return findListSearch;
     }
 }
