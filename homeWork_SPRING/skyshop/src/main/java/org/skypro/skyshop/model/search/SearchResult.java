160fba185ed528ce7dca7c54e32b2354dc02a4d2
Bisha <ogonia07@mail.ru>
2026-02-08 16:40:51 +0300

    интернет-магазин, добавила корзину

--- homeWork_SPRING/skyshop/src/main/java/org/skypro/skyshop/model/search/SearchResult.java
+++ homeWork_SPRING/skyshop/src/main/java/org/skypro/skyshop/model/search/SearchResult.java
@@ -5,27 +5,25 @@ public class SearchResult {
     private final String name;
     private final String contentType;
 
-
     public SearchResult(String id, String name, String contentType) {
         this.id = id;
         this.name = name;
         this.contentType = contentType;
     }
 
-    public String getId(){
+    public String getId() {
         return id;
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
-    public String getContentType(){
+    public String getContentType() {
         return contentType;
     }
 
-    public static SearchResult fromSearchable(Searchable searchable){
+    public static SearchResult fromSearchable(Searchable searchable) {
         return new SearchResult(searchable.getId().toString(), searchable.getSearchTerm(), searchable.getContentType());
     }
-
 }
--- homeWork_SPRING/skyshop/src/main/java/org/skypro/skyshop/service/StorageService.java
+++ homeWork_SPRING/skyshop/src/main/java/org/skypro/skyshop/service/StorageService.java
@@ -15,42 +15,59 @@ public class StorageService {
     private final Map<UUID, Article> mapArticle = new HashMap<>();
 
     public StorageService() {
-        setMapProduct(new SimpleProduct(UUID.randomUUID(), "печенье", 70));
-        setMapProduct(new SimpleProduct(UUID.randomUUID(), "молоко", 50));
-        setMapProduct(new SimpleProduct(UUID.randomUUID(), "мед", 150));
-        setMapProduct(new DiscountedProduct(UUID.randomUUID(), "колбаса", 120, 10));
-        setMapProduct(new DiscountedProduct(UUID.randomUUID(), "кофе", 100, 20));
-        setMapProduct(new DiscountedProduct(UUID.randomUUID(), "кофе растворим", 80, 10));
+        UUID id = UUID.randomUUID();
+        setMapProduct(id, new SimpleProduct(id, "печенье", 70));
+        id = UUID.randomUUID();
+        setMapProduct(id, new SimpleProduct(id, "молоко", 50));
+        id = UUID.randomUUID();
+        setMapProduct(id, new SimpleProduct(id, "мед", 150));
+        id = UUID.randomUUID();
+        setMapProduct(id,new DiscountedProduct(id, "колбаса", 120, 10));
+        id = UUID.randomUUID();
+        setMapProduct(id, new DiscountedProduct(id, "кофе", 100, 20));
+        id = UUID.randomUUID();
+        setMapProduct(id, new DiscountedProduct(id, "кофе растворим", 80, 10));
 
-        setMapArticle(new Article(UUID.randomUUID(), "кофе темной обжарки, с кислинкой", "кофе"));
-        setMapArticle(new Article(UUID.randomUUID(), "кофе автрастворимый. кофе ароматный.", "кофе"));
-        setMapArticle(new Article(UUID.randomUUID(), "кофе авкаапсулах, ароматный, вкусный", "кофе"));
-        setMapArticle(new Article(UUID.randomUUID(), "кофе растворимый, кофе гранулированный, 180гр", "кофе"));
-        setMapArticle(new Article(UUID.randomUUID(), "чай в пакетиках", "чай"));
-        setMapArticle(new Article(UUID.randomUUID(), "чай зеленый", "чай"));
-        setMapArticle(new Article(UUID.randomUUID(), "чай черный", "чай"));
+        id = UUID.randomUUID();
+        setMapArticle(id, new Article(id, "кофе темной обжарки, с кислинкой", "кофе"));
+        id = UUID.randomUUID();
+        setMapArticle(id, new Article(id, "кофе автрастворимый. кофе ароматный.", "кофе"));
+        id = UUID.randomUUID();
+        setMapArticle(id, new Article(id, "кофе авкаапсулах, ароматный, вкусный", "кофе"));
+        id = UUID.randomUUID();
+        setMapArticle(id, new Article(id, "кофе растворимый, кофе гранулированный, 180гр", "кофе"));
+        id = UUID.randomUUID();
+        setMapArticle(id, new Article(id, "чай в пакетиках", "чай"));
+        id = UUID.randomUUID();
+        setMapArticle(id, new Article(id, "чай зеленый", "чай"));
+        id = UUID.randomUUID();
+        setMapArticle(id, new Article(id, "чай черный", "чай"));
     }
 
-    private void setMapProduct(Product product){
-        mapProduct.put(UUID.randomUUID(), product);
+    private void setMapProduct(UUID id, Product product) {
+        mapProduct.put(id, product);
     }
 
-    private void setMapArticle(Article article){
-        mapArticle.put(UUID.randomUUID(), article);
+    private void setMapArticle(UUID id, Article article) {
+        mapArticle.put(id, article);
     }
 
-    public Collection<Product> getMapProduct(){
+    public Collection<Product> getMapProduct() {
         return mapProduct.values();
     }
 
-    public Collection<Article> getMapArticle(){
+    public Collection<Article> getMapArticle() {
         return mapArticle.values();
     }
 
-    public Collection<Searchable> getAllSearcheble(){
+    public Collection<Searchable> getAllSearcheble() {
         List<Searchable> allList = new ArrayList<>();
         allList.addAll(getMapProduct());
         allList.addAll(getMapArticle());
         return allList;
+    }
+    
+    public Optional<Product> getProductById(UUID id) {
+        return Optional.ofNullable(mapProduct.get(id));
     }
 }
