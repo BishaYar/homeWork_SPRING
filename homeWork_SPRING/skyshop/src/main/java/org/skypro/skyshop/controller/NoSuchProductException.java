3d23b40b0665eeae09e45c3399185e1c3951e361
Bisha <ogonia07@mail.ru>
2026-02-12 19:32:42 +0300

    интернет-магазин, добавила свое исключение

---
+++ homeWork_SPRING/skyshop/src/main/java/org/skypro/skyshop/controller/NoSuchProductException.java
@@ -0,0 +1,8 @@
+package org.skypro.skyshop.controller;
+
+public class NoSuchProductException extends RuntimeException {
+
+    public NoSuchProductException(String message) {
+        super(message);
+    }
+}
